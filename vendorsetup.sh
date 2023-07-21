f8e27f74164557d10d9b6cae418e0cb8f27818a8
Darth9 <reyprimerjefe@gmail.com>
2023-07-21 22:57:28 +0100

    Update the build vars; tidy up things

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,17 +37,13 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_ENABLE_APP_MANAGER=1
         export ALLOW_MISSING_DEPENDENCIES=true
 	export TARGET_DEVICE_ALT="begoniain"
-	export OF_TARGET_DEVICES="begoniain,begonia"
+	export FOX_TARGET_DEVICES="begoniain,begonia"
         export OF_SCREEN_H=2340
         export OF_STATUS_H=80
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export FOX_REPLACE_BUSYBOX_PS=1
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 	export OF_FBE_METADATA_MOUNT_IGNORE=1
-        export OF_USE_SYSTEM_FINGERPRINT=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_NANO_EDITOR=1
@@ -56,6 +52,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export OF_USE_GREEN_LED=0
+
+	# patch avb20 - some ROM recoveries try to overwrite custom recoveries
+	export OF_PATCH_AVB20=1
 
 	# quick backup defaults
         export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
@@ -67,16 +66,16 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 	# necessary to decrypt most begonia ROMs
 	export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+	export OF_LEGACY_PROCESS_FSTAB=1
 
        	# ensure that /sdcard is bind-unmounted before f2fs data repair or format
        	export OF_UNBIND_SDCARD_F2FS=1
 
-	# dispense with all the MIUI OTA stuff
-	export OF_VANILLA_BUILD=1
-	export OF_DISABLE_OTA_MENU=1
-
 	# flashlight doesn't work
 	export OF_FLASHLIGHT_ENABLE=0
+
+	# no MIUI stuff
+	export OF_DISABLE_OTA_MENU=1
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
@@ -85,5 +84,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
 fi
 #
